e1b46f34444890e3abd58e08f97cd86e15ce2216
Zakaria <zakousama15@gmail.com>
2023-01-09 03:17:38 +0100

    fullscreen acceuil duh

--- HistoriqueAchats/src/main/java/com/example/historiqueachats/HelloApplication.java
+++ HistoriqueAchats/src/main/java/com/example/historiqueachats/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.acceuil;
+package com.example.historiqueachats;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("page2.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 410);
-        stage.setTitle("Acceuil");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HistoriqueA.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
--- page8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.page8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.page8 to javafx.fxml;
-    exports com.example.page8;
-}
