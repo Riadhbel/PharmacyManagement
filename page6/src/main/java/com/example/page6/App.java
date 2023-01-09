bf29a4cb41d1a707ae7a21666922b6e5297fb8e5
halimabnk <115838084+halimabnk@users.noreply.github.com>
2023-01-09 13:20:17 +0100

    Add files via upload

---
+++ page6/src/main/java/com/example/page6/App.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*; 
+import javafx.stage.Stage;
+public class App extends Application{
+   
+    @Override
+public void start(Stage primaryStage){
+     Parent root;
+    try {
+        root = FXMLLoader.load(getClass().getResource("page6.fxml"));
+        Scene scene = new Scene(root,600,400);
+        root.getStylesheets().add("style6.css");
+        primaryStage.setTitle("Creation achat");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+
+    } catch (IOException e) {
+        
+        e.printStackTrace();
+    }
+    
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    }
